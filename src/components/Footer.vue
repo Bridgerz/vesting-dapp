6f23f2263531d199a3bdb4cbc61ecf178ffe266b
bridgerz <bridgerzoske@gmail.com>
2021-11-08 23:34:36 -0800

    REAL initial 🚀

---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <h5 class="display-5">@{{ year }} - ReSource</h5>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+  data: () => ({
+    year: new Date().getFullYear(),
+  }),
+};
+</script>
